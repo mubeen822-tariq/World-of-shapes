45d8fa233490626c12f1b6cc07451d0cd394ccfc
mubeen822-tariq <mubeen-tariq@users.noreply.github.com>
2021-04-14 13:34:13 +0500

    scroling issue

---
+++ app/src/main/java/com/example/worldofshapes/Splash.java
@@ -0,0 +1,32 @@
+package com.example.worldofshapes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+    private ImageView logo;
+    private static int splashTimeOut=5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo=(ImageView)findViewById(R.id.imageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
