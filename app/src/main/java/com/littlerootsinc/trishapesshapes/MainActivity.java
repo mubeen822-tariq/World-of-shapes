0c479f13b4c9a6794a674bf21d27174e04381dd2
MubeenZamstudios <83269660+MubeenZamstudios@users.noreply.github.com>
2021-12-16 13:42:03 +0500

    change package name change app name add aap file and key and password

---
+++ app/src/main/java/com/littlerootsinc/trishapesshapes/MainActivity.java
@@ -0,0 +1,206 @@
+package com.littlerootsinc.trishapesshapes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.littlerootsinc.trishapesshapes.Helper.CenterZoomLayoutManager;
+import com.littlerootsinc.worldofshapes.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private RecyclerView shapesRecycler;
+    private List<ImageItem> imageItemList;
+    private ShapesAdapter adapter;
+    ImageView home;
+
+    private CenterZoomLayoutManager centerZoomLayoutManager;
+
+//    private AdView adView;
+
+    private Button previous, play, next;
+    private int counter = 0;
+
+    private MediaPlayer mediaPlayer;
+    private int[] sounds;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Setting up the activity for full screen mode
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        sounds = new int[]{R.raw.circle, R.raw.triangle, R.raw.sphere, R.raw.square, R.raw.rectangle, R.raw.hexagon,
+                R.raw.pentagon, R.raw.cylinder, R.raw.cube, R.raw.pyramid, R.raw.cone};
+
+        /*adView = (AdView) findViewById(R.id.shapes_ad);
+        AdRequest adRequest = new AdRequest.Builder()
+                .tagForChildDirectedTreatment(true)
+                .build();
+        adView.loadAd(adRequest);*/
+        home = (ImageView) findViewById(R.id.logout);
+
+        home.bringToFront();
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(MainActivity.this, R.style.UpdateDialogStyle));
+                /*  builder.setTitle(R.string.app_name);*/
+
+                builder.setMessage("Do you want to exit?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+
+        imageItemList = new ArrayList<>();
+        initList();
+        adapter = new ShapesAdapter(this, imageItemList);
+
+        shapesRecycler = (RecyclerView) findViewById(R.id.recycler_shapes);
+        shapesRecycler.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+                counter = centerZoomLayoutManager.findLastCompletelyVisibleItemPosition();
+                int pos = counter % imageItemList.size();
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+                if (pos>=0){
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), sounds[pos]);
+                    mediaPlayer.start();
+
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+        centerZoomLayoutManager = new CenterZoomLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        shapesRecycler.setLayoutManager(centerZoomLayoutManager);
+        shapesRecycler.setItemAnimator(new DefaultItemAnimator());
+        shapesRecycler.setAdapter(adapter);
+
+
+        previous = (Button) findViewById(R.id.previous_shapes);
+        play = (Button) findViewById(R.id.play_shapes);
+        next = (Button) findViewById(R.id.next_shapes);
+
+        counter = Integer.MAX_VALUE / 2;
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = centerZoomLayoutManager.findLastCompletelyVisibleItemPosition();
+                counter--;
+                shapesRecycler.smoothScrollToPosition(counter);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = centerZoomLayoutManager.findLastCompletelyVisibleItemPosition();
+                counter++;
+                shapesRecycler.smoothScrollToPosition(counter);
+            }
+        });
+
+        shapesRecycler.scrollToPosition(counter);
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(shapesRecycler);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = centerZoomLayoutManager.findLastCompletelyVisibleItemPosition();
+                int pos = counter % imageItemList.size();
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+                if (pos>=0){
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), sounds[pos]);
+                    mediaPlayer.start();
+                }else
+                    shapesRecycler.smoothScrollToPosition(0);
+            }
+        });
+    }
+
+
+
+    private void initList() {
+        imageItemList.add(new ImageItem("Circle", R.drawable.circle));
+        imageItemList.add(new ImageItem("Triangle", R.drawable.triangle));
+        imageItemList.add(new ImageItem("Sphere", R.drawable.sphere));
+        imageItemList.add(new ImageItem("Square", R.drawable.square));
+        imageItemList.add(new ImageItem("Rectangle", R.drawable.rectangle));
+        imageItemList.add(new ImageItem("Hexagon", R.drawable.hexagon));
+        imageItemList.add(new ImageItem("Pentagon", R.drawable.pentagon));
+        imageItemList.add(new ImageItem("Cylinder", R.drawable.cylinder));
+        imageItemList.add(new ImageItem("Cube", R.drawable.cube));
+        imageItemList.add(new ImageItem("Pyramid", R.drawable.pyramid));
+        imageItemList.add(new ImageItem("Cone", R.drawable.cone));
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        counter = centerZoomLayoutManager.findLastCompletelyVisibleItemPosition();
+        int pos = counter % imageItemList.size();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), sounds[pos]);
+        mediaPlayer.start();
+        Toast.makeText(this, "check that", Toast.LENGTH_SHORT).show();
+    }
+}
